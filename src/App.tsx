067fe40503e98760a8cf83a9b3d6ae78856344fe
Omar-Said <omar.a.said@outlook.com>
2021-10-24 21:34:23 -0400

    added redux boiler plate with single api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Home from "./screens/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <Home />
       </header>
     </div>
   );
